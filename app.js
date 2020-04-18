c56944ce3906ee86030911e0c70fe43307d8d20a
Jaime Daza <jaimedaza11@gmail.com>
2020-04-17 19:29:13 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/visitantes2', {
+	useNewUrlParser: true,	
+});
+
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+
+const schema = new mongoose.Schema({
+   
+    name: { type: String, default: "Anónimo" },
+    count: { type: Number, default: 1 }
+  
+});
+
+const Visitor = mongoose.model('Visitor2', schema);
+
+app.get('/', (req, res) => { 
+
+	let name2 = req.query.name;	
+
+	Visitor.find({ name: name2 }, function(err, arr) {
+		if(err){
+			return console.log(err);
+
+		//SI VIENE EL NOMBRE VACIO ""
+		}else if(name2 == ""){
+			name2 = "Anónimo";
+
+			const person = new Visitor({		
+				name: name2,
+				count: 1
+			});
+
+			person.save(function(error){
+				return console.log(error);				
+			});
+
+		//SI NO VIENE NOMBRE O SI ES PRIMERA VEZ DEL VISITANTE
+		}else if( arr.length === 0){
+			
+			const person = new Visitor({		
+				name: name2,
+				count: 1
+			});
+
+			person.save(function(error){
+				return console.log(error);				
+			});
+
+		//SI ES UN VISITANTE RECURRENTE
+		}else{
+
+			Visitor.update({ name: name2 }, { $inc: {count: 1 }}, function(err) {
+			  if (err) return console.error(err);
+			});
+
+			console.log(arr);
+			return arr;
+		}
+	})
+
+	//CREACION DE TABLA PARA RESULTADOS	
+
+	let HTML = "";
+	HTML+= "<table>";
+		HTML+= "<tr><th>Id</th><th>Name</th><th>Visits</th></tr>";
+
+
+	Visitor.find(function(err, visitors) {
+	  if (err) return console.error(err);	  
+
+	  visitors.forEach(function(vis){
+	  	HTML+= '<tr><td>'+vis["_id"]+'</td><td>'+vis["name"]+'</td><td>'+vis["count"]+'</td></tr>';	  	  	
+	  });
+
+	  HTML+= "</table>";
+	  
+	  res.send(HTML);
+	});	
+	
+
+});
+
+
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
